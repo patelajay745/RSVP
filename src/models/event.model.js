b690b0b2aa6a7e6bbc4dce316555d5d03d97530f
Ajay patel <patel.ajay745@gmail.com>
2024-09-23 15:37:21 -0400

    added all remainig models

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const { nanoid } = require("nanoid");
 
 const eventSchema = new Schema(
     {
@@ -7,6 +8,54 @@ const eventSchema = new Schema(
             type: String,
             required: true,
             maxLength: 20,
+        },
+        startDate: {
+            type: Date,
+            required: true,
+        },
+        startTime: {
+            type: String,
+            required: true,
+        },
+        endDate: {
+            type: Date,
+            required: true,
+        },
+        endTime: {
+            type: String,
+            required: true,
+        },
+        timezone: {
+            type: String,
+            required: true,
+        },
+        venueName: {
+            type: String,
+            required: true,
+        },
+        venueAddress: {
+            type: String,
+            required: true,
+        },
+        themephoto: {
+            type: String,
+            required: true,
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        status: {
+            type: String,
+            enum: ["published", "closed"],
+            default: "closed",
+        },
+        shortUrl: {
+            type: String,
+            unique: true,
+            default: () => {
+                return nanoid(6);
+            },
         },
     },
     { timestamps: true }
---
+++ src/models/family.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const familySchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        numAttendees: {
+            type: Number,
+            default: 0,
+        },
+    },
+    { timestamps: true }
+);
+
+const Family = mongoose.model("Family", familySchema);
+
+module.exports = { Family };
