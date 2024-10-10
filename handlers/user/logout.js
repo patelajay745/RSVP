44b4a97d316db7bcb6bef33144d2914a70b6f8d4
Ajay patel <patel.ajay745@gmail.com>
2024-10-10 18:41:39 -0400

    added middleware

--- handlers/user/logout.js
+++ handlers/user/logout.js
@@ -1,1 +1,33 @@
-module.exports.handler = async (event) => {};
+const { User } = require("../../models/user.model");
+const { ApiResponse } = require("../../utils/ApiResponse");
+
+module.exports.handler = async (event, context) => {
+    // console.log("context value", context);
+    console.log("context value", context.user._id);
+
+    const userId = context.user._id;
+
+    await User.findByIdAndUpdate(
+        userId,
+        {
+            $unset: {
+                refreshToken: 1,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    throw new ApiResponse(
+        200,
+        "user logged out",
+        {},
+        {
+            "Set-Cookie": [
+                `accessToken=; HttpOnly; Path=/;`,
+                `refreshToken=; HttpOnly; Path=/;`,
+            ],
+        }
+    );
+};
---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/user.model");
+const { ApiResponse } = require("../utils/ApiResponse");
+const connectDb = require("../db");
+
+connectDb(process.env.MONGODB_URI);
+
+module.exports.authenticate = async (event, context) => {
+    const cookies = event.headers.Cookie;
+    let accessToken;
+    if (cookies) {
+        const cookieArray = cookies.split(";");
+        for (let cookie of cookieArray) {
+            const [name, value] = cookie.trim().split("=");
+            if (name === "accessToken") {
+                if (value != "{}") accessToken = value;
+                break;
+            }
+        }
+    }
+
+    const authHeader = event.headers.Authorization;
+    const tokenFromHeader = authHeader ? authHeader.split("Bearer ")[1] : null;
+
+    const token = accessToken || tokenFromHeader;
+
+    if (!token) {
+        throw new ApiResponse(401, "Unauthorized request");
+    }
+
+    const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findOne({ _id: decodeToken._id }).select(
+        "-password -__v -createdAt -updatedAt"
+    );
+
+    if (!user) {
+        throw new ApiResponse(401, "invalid Token");
+    }
+
+    context.user = user;
+};
