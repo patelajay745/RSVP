d7409b6626df759700adaee7f8af6f3e00ed0cc6
Ajay patel <patel.ajay745@gmail.com>
2024-10-16 16:57:28 -0400

    added getAllEvents

---
+++ handlers/event/getAllEvents.js
@@ -0,0 +1,16 @@
+const { ApiResponse } = require("../../utils/ApiResponse");
+const connectDb = require("../../db");
+const { Event } = require("../../models/event.model");
+connectDb(process.env.MONGODB_URI);
+
+module.exports.handler = async (event, context) => {
+    const userID = context.user._id;
+
+    const requestedEvents = await Event.find({ createdBy: userID });
+
+    if (!requestedEvents) {
+        return new ApiResponse(400, "No Events found");
+    }
+
+    return new ApiResponse(200, requestedEvents, "Events are fetched");
+};
